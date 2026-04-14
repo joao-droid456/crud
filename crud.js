31ee7abb38d169b177579f378d5c52424471ef53
joao-droid456 <joao.v.chagas12@aluno.senai.br>
2026-04-14 09:11:55 -0300

    Update crud.js

--- crud.js
+++ crud.js
@@ -1,28 +1,28 @@
 import { db } from "./firebaseConfig.js";
 import { ref, push, onValue, update, remove } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-database.js";
 
-// CREATE
+
 export const createCharacter = (character) => {
   const charactersRef = ref(db, "characters");
   push(charactersRef, character);
 };
 
-// READ em tempo real
+
 export const onGetCharacters = (callback) => {
   const charactersRef = ref(db, "characters");
   onValue(charactersRef, (snapshot) => {
-    const data = snapshot.val() || {}; // retorna {} se null
+    const data = snapshot.val() || {}; 
     callback(data);
   });
 };
 
-// UPDATE
+
 export const updateCharacter = (id, character) => {
   const charRef = ref(db, "characters/" + id);
   update(charRef, character);
 };
 
-// DELETE
+
 export const deleteCharacter = (id) => {
   const charRef = ref(db, "characters/" + id);
   remove(charRef);
